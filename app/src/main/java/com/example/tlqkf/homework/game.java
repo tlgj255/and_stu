b0386099d6e5bc8e077092f404ded3c8f1f30e6a
tlgj255 <tlgj255@naver.com>
2016-09-29 10:28:05 +0900

    shorter than before (use array)

--- app/src/main/java/com/example/tlqkf/homework/game.java
+++ app/src/main/java/com/example/tlqkf/homework/game.java
@@ -25,13 +25,16 @@ import android.widget.Toast;
 public class game extends AppCompatActivity {
     Animation check;
     int a = 0, Random, Random2, Random3, b[] = new int[10], c = 0, g = 10, s1 = 0, s2 = 0, s3 = 0, s = 0, ball = 0, dw = 0;
-    TextView n_f1, n_f2, n_f3, life, count, n_f1_, n_f3_, n_f4_, n_f2_, dab, dab_, dab__, R1, R2, R3, R4, R5, R6, R7, R8, R9;
-    Button n_1, n_2, n_3, n_4, n_5, n_6, n_7, n_8, n_9, menu, yes, no, ok, back, out, regame, re_, bye, good;
+    TextView n_f1, n_f2, n_f3, life, count, dab, dab_, dab__;
+    Button menu, yes, no, ok, back, out, regame, re_, bye, good;
     RelativeLayout ask, clear, die, record;
     private Handler mHandler;
     EditText name;
     database db;
      MediaPlayer ppyok,pyororo,forgive;
+    private TextView [] ani /*animatin*/ = new TextView[4];
+    private TextView [] Rn/*round*/ = new TextView[9];
+    private Button [] n = new Button[9];
     @Override
     public void onBackPressed() {
         if (a == 1) {
@@ -78,22 +81,22 @@ public class game extends AppCompatActivity {
         mHandler = new Handler();
         record = (RelativeLayout) findViewById(R.id.record);
         good = (Button) findViewById(R.id.good);
-        R1 = (TextView) findViewById(R.id.textView2);
-        R2 = (TextView) findViewById(R.id.textView3);
-        R3 = (TextView) findViewById(R.id.textView4);
-        R4 = (TextView) findViewById(R.id.textView5);
-        R5 = (TextView) findViewById(R.id.textView6);
-        R6 = (TextView) findViewById(R.id.textView7);
-        R7 = (TextView) findViewById(R.id.textView8);
-        R8 = (TextView) findViewById(R.id.textView9);
-        R9 = (TextView) findViewById(R.id.textView10);
+        Rn[0] = (TextView) findViewById(R.id.textView2);
+        Rn[1] = (TextView) findViewById(R.id.textView3);
+        Rn[2] = (TextView) findViewById(R.id.textView4);
+        Rn[3] = (TextView) findViewById(R.id.textView5);
+        Rn[4] = (TextView) findViewById(R.id.textView6);
+        Rn[5] = (TextView) findViewById(R.id.textView7);
+        Rn[6] = (TextView) findViewById(R.id.textView8);
+        Rn[7] = (TextView) findViewById(R.id.textView9);
+        Rn[8] = (TextView) findViewById(R.id.textView10);
         dab = (TextView) findViewById(R.id.f_n1__);
         dab_ = (TextView) findViewById(R.id.f_n2__);
         dab__ = (TextView) findViewById(R.id.f_n3__);
-        n_f4_ = (TextView) findViewById(R.id.f_n4_);
-        n_f3_ = (TextView) findViewById(R.id.f_n3_);
-        n_f2_ = (TextView) findViewById(R.id.f_n2_);
-        n_f1_ = (TextView) findViewById(R.id.f_n1_);
+       ani[0] = (TextView) findViewById(R.id.f_n4_);
+       ani[1] = (TextView) findViewById(R.id.f_n3_);
+       ani[2] = (TextView) findViewById(R.id.f_n2_);
+       ani[3] = (TextView) findViewById(R.id.f_n1_);
         count = (TextView) findViewById(R.id.count);
         clear = (RelativeLayout) findViewById(R.id.clear);
         out = (Button) findViewById(R.id.out);
@@ -104,15 +107,15 @@ public class game extends AppCompatActivity {
         n_f1 = (TextView) findViewById(R.id.f_n1);
         n_f2 = (TextView) findViewById(R.id.f_n2);
         n_f3 = (TextView) findViewById(R.id.f_n3);
-        n_1 = (Button) findViewById(R.id.button6);
-        n_2 = (Button) findViewById(R.id.button4);
-        n_3 = (Button) findViewById(R.id.button7);
-        n_4 = (Button) findViewById(R.id.button5);
-        n_5 = (Button) findViewById(R.id.button8);
-        n_6 = (Button) findViewById(R.id.button9);
-        n_7 = (Button) findViewById(R.id.button10);
-        n_8 = (Button) findViewById(R.id.button11);
-        n_9 = (Button) findViewById(R.id.button12);
+        n[0] = (Button) findViewById(R.id.button6);
+        n[1] = (Button) findViewById(R.id.button4);
+        n[2] = (Button) findViewById(R.id.button7);
+        n[3] = (Button) findViewById(R.id.button5);
+        n[4] = (Button) findViewById(R.id.button8);
+        n[5] = (Button) findViewById(R.id.button9);
+        n[6] = (Button) findViewById(R.id.button10);
+        n[7] = (Button) findViewById(R.id.button11);
+        n[8] = (Button) findViewById(R.id.button12);
         menu = (Button) findViewById(R.id.button13);
         ask = (RelativeLayout) findViewById(R.id.ask);
         yes = (Button) findViewById(R.id.finish);
@@ -120,7 +123,7 @@ public class game extends AppCompatActivity {
         Random = (int) (Math.random() * 9) + 1;
         Random2 = (int) (Math.random() * 9) + 1;
         Random3 = (int) (Math.random() * 9) + 1;
-        while (true) {//랜덤수가 중복되지 않을때까지 돌리기
+        while (true) {//get random number while not duplicate that
             if (Random == Random2 || Random == Random3 || Random2 == Random3) {
                 Random = (int) (Math.random() * 9) + 1;
                 Random2 = (int) (Math.random() * 9) + 1;
@@ -128,7 +131,7 @@ public class game extends AppCompatActivity {
             } else {
                 break;
             }
-        }//랜덤수 3개
+        }//three random number
         Log.i("Random", "" + Random);
         Log.i("Random2", "" + Random2);
         Log.i("Random3", "" + Random3);
@@ -142,7 +145,7 @@ public class game extends AppCompatActivity {
         toast = Toast.makeText(game.this, "숫자를 마저 입력해주세요", Toast.LENGTH_SHORT);
         Intent I = new Intent(this, MainActivity.class);
         switch (v.getId()) {
-            case R.id.button13://버튼를 눌렀을때 메뉴로 돌아갈건지 물어보는 레이아웃 띄우기,버튼 비활성화
+            case R.id.button13://if you press the button, show menu go MainActivity or not
                 a = 1;
                 ask.setVisibility(View.VISIBLE);
                 yes.setClickable(true);
@@ -168,39 +171,39 @@ public class game extends AppCompatActivity {
                 break;
             case R.id.button6:
                 b[c] = 1;
-                n_1.setClickable(false);
+                n[0].setClickable(false);
                 break;
             case R.id.button4:
                 b[c] = 6;
-                n_2.setClickable(false);
+                n[1].setClickable(false);
                 break;
             case R.id.button7:
                 b[c] = 2;
-                n_3.setClickable(false);
+                n[2].setClickable(false);
                 break;
             case R.id.button5:
                 b[c] = 7;
-                n_4.setClickable(false);
+                n[3].setClickable(false);
                 break;
             case R.id.button8:
                 b[c] = 3;
-                n_5.setClickable(false);
+                n[4].setClickable(false);
                 break;
             case R.id.button9:
                 b[c] = 8;
-                n_6.setClickable(false);
+                n[5].setClickable(false);
                 break;
             case R.id.button10:
                 b[c] = 4;
-                n_7.setClickable(false);
+                n[6].setClickable(false);
                 break;
             case R.id.button11:
                 b[c] = 9;
-                n_8.setClickable(false);
+                n[7].setClickable(false);
                 break;
             case R.id.button12:
                 b[c] = 5;
-                n_9.setClickable(false);
+                n[8].setClickable(false);
                 break;
             case R.id.button14:
                 if (c >= 3) {
@@ -224,71 +227,34 @@ public class game extends AppCompatActivity {
                         count.setText(11 - g + "번만에 클리어!!");
                         dw++;
                     } else {//클리어 못했을 시
-                        pyororo.start();
-                        n_f4_.setVisibility(View.VISIBLE);//애니매이션 나타내기
-                        n_f4_.startAnimation(check);//애니매이션 나타내기
-                        n_f3_.setVisibility(View.VISIBLE);//애니매이션 나타내기
-                        n_f3_.startAnimation(check);//애니매이션 나타내기
-                        n_f2_.setVisibility(View.VISIBLE);//애니매이션 나타내기
-                        n_f2_.startAnimation(check);//애니매이션 나타내기
-                        n_f1_.setVisibility(View.VISIBLE);//애니매이션 나타내기
-                        n_f1_.startAnimation(check);//애니매이션 나타내기
-                        n_f4_.setText("" + s);
-                        n_f2_.setText("" + ball);
+                        pyororo.start();//start sound
+                        for(int i = 0; i < 4; i++){
+                            ani[i].setVisibility(View.VISIBLE);//애니매이션 나타내기
+                            ani[i].startAnimation(check);//애니매이션 나타내기
+                        }
+                        ani[3].setText("" + s);
+                        ani[1].setText("" + ball);
                         new Handler().postDelayed(new Runnable() {
                             @Override
                             public void run() {
-                                n_f4_.setVisibility(View.GONE);
-                                n_f1_.setVisibility(View.GONE);
-                                n_f2_.setVisibility(View.GONE);
-                                n_f3_.setVisibility(View.GONE);
+                                for(int i = 0; i < 4; i++)
+                                ani[i].setVisibility(View.GONE);
                                 mHandler.sendEmptyMessage(0);
                             }
-                        }, 2200);//2.2초후에 위에 명령어들 실행
+                        }, 2200);//animation gone after 2.2s
                         Handler mHandler = new Handler() {
                             public void handleMessage(Message msg) {
                             }
                         };
-                        if (g == 10) {//화면에 라운드,스트라이크,볼 수 나타내기
-                            R1.setText("1R " + s + "S " + ball + "B\n   " + b[0] + " " + b[1] + " " + b[2]);
-                            R1.setVisibility(View.VISIBLE);
-                        } else if (g == 9) {
-                            R2.setText("2R " + s + "S " + ball + "B\n   " + b[0] + " " + b[1] + " " + b[2]);
-                            R2.setVisibility(View.VISIBLE);
-                        } else if (g == 8) {
-                            R3.setText("3R " + s + "S " + ball + "B\n   " + b[0] + " " + b[1] + " " + b[2]);
-                            R3.setVisibility(View.VISIBLE);
-                        } else if (g == 7) {
-                            R4.setText("4R " + s + "S " + ball + "B\n   " + b[0] + " " + b[1] + " " + b[2]);
-                            R4.setVisibility(View.VISIBLE);
-                        } else if (g == 6) {
-                            R5.setText("5R " + s + "S " + ball + "B\n   " + b[0] + " " + b[1] + " " + b[2]);
-                            R5.setVisibility(View.VISIBLE);
-                        } else if (g == 5) {
-                            R6.setText("6R " + s + "S " + ball + "B\n   " + b[0] + " " + b[1] + " " + b[2]);
-                            R6.setVisibility(View.VISIBLE);
-                        } else if (g == 4) {
-                            R7.setText("7R " + s + "S " + ball + "B\n   " + b[0] + " " + b[1] + " " + b[2]);
-                            R7.setVisibility(View.VISIBLE);
-                        } else if (g == 3) {
-                            R8.setText("8R " + s + "S " + ball + "B\n   " + b[0] + " " + b[1] + " " + b[2]);
-                            R8.setVisibility(View.VISIBLE);
-                        } else if (g == 2) {
-                            R9.setText("9R " + s + "S " + ball + "B\n   " + b[0] + " " + b[1] + " " + b[2]);
-                            R9.setVisibility(View.VISIBLE);
-                        }
+                        //show round and strike and ball on interface
+                            Rn[10-g].setText("1R " + s + "S " + ball + "B\n   " + b[0] + " " + b[1] + " " + b[2]);
+                            Rn[10-g].setVisibility(View.VISIBLE);
                         b[0] = 0;
                         b[1] = 0;
                         b[2] = 0;
-                        n_1.setClickable(true);
-                        n_2.setClickable(true);
-                        n_3.setClickable(true);
-                        n_4.setClickable(true);
-                        n_5.setClickable(true);
-                        n_6.setClickable(true);
-                        n_7.setClickable(true);
-                        n_8.setClickable(true);
-                        n_9.setClickable(true);
+                        for(int i = 0; i < 9; i++){
+                            n[i].setClickable(true);
+                        }
                         c = -1;
                         g--;
                         if (g > 0)
@@ -337,57 +303,30 @@ public class game extends AppCompatActivity {
                 }
             case R.id.button15://지우기 키를 눌렀을때 클릭이 비활성화 돼있는 것을 활성화시키는 작업
                 if (c == 1) {
-                    n_1.setClickable(true);
-                    n_2.setClickable(true);
-                    n_3.setClickable(true);
-                    n_4.setClickable(true);
-                    n_5.setClickable(true);
-                    n_6.setClickable(true);
-                    n_7.setClickable(true);
-                    n_8.setClickable(true);
-                    n_9.setClickable(true);
+                    for(int i = 0; i < 9; i++){
+                        n[i].setClickable(true);
+                    }
                     c = c - 2;
                     b[0] = 0;
                 } else if (c == 2) {
-                    if (b[1] == 1) n_1.setClickable(true);
-                    if (b[1] == 2) n_3.setClickable(true);
-                    if (b[1] == 3) n_5.setClickable(true);
-                    if (b[1] == 4) n_7.setClickable(true);
-                    if (b[1] == 5) n_9.setClickable(true);
-                    if (b[1] == 6) n_2.setClickable(true);
-                    if (b[1] == 7) n_4.setClickable(true);
-                    if (b[1] == 8) n_6.setClickable(true);
-                    if (b[1] == 9) n_8.setClickable(true);
+                    for(int i = 0; i < 9; i++){
+                        if(b[1] == i)
+                        n[i].setClickable(true);
+                    }
                     b[1] = 0;
                     c = c - 2;
                 } else if (c == 3) {
-                    n_1.setClickable(true);
-                    n_2.setClickable(true);
-                    n_3.setClickable(true);
-                    n_4.setClickable(true);
-                    n_5.setClickable(true);
-                    n_6.setClickable(true);
-                    n_7.setClickable(true);
-                    n_8.setClickable(true);
-                    n_9.setClickable(true);
-                    if (b[1] == 1) n_1.setClickable(false);
-                    if (b[1] == 2) n_3.setClickable(false);
-                    if (b[1] == 3) n_5.setClickable(false);
-                    if (b[1] == 4) n_7.setClickable(false);
-                    if (b[1] == 5) n_9.setClickable(false);
-                    if (b[1] == 6) n_2.setClickable(false);
-                    if (b[1] == 7) n_4.setClickable(false);
-                    if (b[1] == 8) n_6.setClickable(false);
-                    if (b[1] == 9) n_8.setClickable(false);
-                    if (b[0] == 1) n_1.setClickable(false);
-                    if (b[0] == 2) n_3.setClickable(false);
-                    if (b[0] == 3) n_5.setClickable(false);
-                    if (b[0] == 4) n_7.setClickable(false);
-                    if (b[0] == 5) n_9.setClickable(false);
-                    if (b[0] == 6) n_2.setClickable(false);
-                    if (b[0] == 7) n_4.setClickable(false);
-                    if (b[0] == 8) n_6.setClickable(false);
-                    if (b[0] == 9) n_8.setClickable(false);
+                    for(int i = 0; i < 9; i++){
+                        n[i].setClickable(true);
+                    }
+                    for(int i = 0; i < 9; i++){
+                        if(b[1] == i)
+                            n[i].setClickable(false);
+                    }
+                    for(int i = 0; i < 9; i++){
+                        if(b[0] == i)
+                            n[i].setClickable(false);
+                    }
                     b[2] = 0;
                     c = c - 2;
                 } else
@@ -412,15 +351,9 @@ public class game extends AppCompatActivity {
         else
         c = 3;
         if (c == 3) {//숫자를 3번 입력시 클릭 비활성화
-            n_1.setClickable(false);
-            n_2.setClickable(false);
-            n_3.setClickable(false);
-            n_4.setClickable(false);
-            n_5.setClickable(false);
-            n_6.setClickable(false);
-            n_7.setClickable(false);
-            n_8.setClickable(false);
-            n_9.setClickable(false);
+            for(int i = 0; i < 9; i++){
+                    n[i].setClickable(false);
+            }
         }
     }
 }
